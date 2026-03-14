7eafea5693bb2dbd6897239b759150ccda5b0d88
kingpin9292 <jakarea9292@gmail.com>
2026-03-13 21:45:49 -0400

    feat: initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h1>home</h1>
+      <h1 className="text-3xl font-bold underline text-white">home</h1>
     </div>
   );
 }
